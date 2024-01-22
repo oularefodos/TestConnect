35ce2d9c5ded08073dff48f1eb868b9943bcd1d9
Fode Oulare <foulare@e1r3p8.1337.ma>
2024-01-22 15:24:57 +0100

    add a condition to treat ZodError

--- server/src/api/middleware/assync.Wrapper.js
+++ server/src/api/middleware/assync.Wrapper.js
@@ -1,10 +1,17 @@
+const { ZodError } = require("zod");
+
 const asyncWrapper = (fnct) => {
     return async (req, res, next) => {
         try {
             await fnct(req, res, next);
         }
-        catch (error) { 
-            res.status(500).json({message : 'something went wrong'});
+        catch (error) {
+            if (error instanceof ZodError) {
+                res.status(401).json({message : 'wrong incommig datas'});
+            }
+            else {
+                res.status(500).json({message : 'something went wrong'});
+            }
         }
     }
 }
