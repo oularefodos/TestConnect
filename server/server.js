803b0da49742a406bbdcd4d3891efa465f95889e
Fode Oulare <foulare@e1r3p8.1337.ma>
2024-01-22 13:17:22 +0100

    Create server: set up initial Express server in server.js and src/app.js

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const app = require('./src/app');
+const dotenv = require('dotenv')
+dotenv.config();
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
