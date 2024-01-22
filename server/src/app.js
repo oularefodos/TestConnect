ad03597068223a5251c58dc9677f8bc6085a3faf
Fode Oulare <foulare@e1r3p8.1337.ma>
2024-01-22 15:29:30 +0100

    set up the rute for user routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors');
 const notFound = require('./api/middleware/notFoundError');
+const userRoutes = require('./api/routes/user.routes')
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+
+// routes
+app.use('/api/users', userRoutes);
 
 
 // Error handler 
